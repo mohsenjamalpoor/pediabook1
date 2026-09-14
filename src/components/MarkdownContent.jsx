c7fc1992db5dceac30ff09f042c9bb0f6f73430d
m.jamalpour <mohsenjamal1023@gmail.com>
2026-09-14 14:05:12 +0330

    add

--- src/components/MarkdownContent.jsx
+++ src/components/MarkdownContent.jsx
@@ -1,10 +1,13 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownContent({ content }) {
   return (
-    <div className="clinic-content tnum">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    <div className="clinic-content tnum" dir="rtl">
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
